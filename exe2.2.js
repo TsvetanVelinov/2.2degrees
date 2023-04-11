38d1f0bf543333f0ceb11735b50635e37e024b3f
Velinov <124807884+TsvetanVelinov@users.noreply.github.com>
2023-04-12 00:02:33 +0300

    exe2.2
    Radians to degrees

---
+++ exe2.2.js
@@ -0,0 +1,12 @@
+function radiansToDegrees (input){
+
+    let radians = Number (input[0]);
+
+    let gradus = radians*180/Math.PI
+
+
+    console.log(gradus);
+
+}
+
+radiansToDegrees (["3.1416"])
